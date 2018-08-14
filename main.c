294d5d722c713e90f1034c1bb64743d9a35f2cc3
Rubsgo <42361623+Rubsgo@users.noreply.github.com>
2018-08-13 20:34:06 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>  /*  printf(char*, ...) Este es un comentario de Bloque :D */
+#include <stdlib.h> //comentario de una sola linea//
+
+int main(int argc, char *argv[])
+{
+  putchar('A');
+  putchar('\n');
+  putchar('\n');
+  printf("HOLA MUNDO C!!!\n");
+  system("PAUSE");	
+  return 0;
+}
